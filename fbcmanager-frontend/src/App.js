d4a26f90f1bd1fe0e4e2c7f90d83bf71ba8cf22e
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2022-02-17 13:02:08 +0100

    added node_modules to git ignore

--- fbcmanager-frontend/src/App.js
+++ fbcmanager-frontend/src/App.js
@@ -1,11 +1,5 @@
 import React, { Component, useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
-import CreateUser from "./api/createUser";
-import GetAllUsers from "./api/getAllUsers";
-import DeleteUser from "./api/deleteUser";
-import GetUser from "./api/getUser";
-import UpdateUser from "./api/updateUser";
+
 
 
 
@@ -13,17 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <UserProfiles /> */}
-        {/* <PostUser/> */}
-        <CreateUser/>
-        <br></br>
-        <GetAllUsers/>
-        <br></br>
-        <DeleteUser/>
-        <br></br>
-        <GetUser/>
-        <br></br>
-        <UpdateUser/>
+        <h1>Test</h1>
       </div>
     );
   }
