4470de9b81ef3d1367e21b20504aa347f258c153
Kongsbak99 <victoreskongsbak@gmail.com>
2022-02-25 10:35:11 +0100

    scss added

--- fbcmanager-frontend/src/pages/forside.js
+++ fbcmanager-frontend/src/pages/forside.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../style.css";
-import "../css/forside.css"
+//import "../css/forside.css"
+import "../scss/forside.scss"
 
 
 
