18c5bae0a9c36657f969ab191fb22345dd49a27f
MarieSeindal <s185363@student.dtu.dk>
2022-03-10 17:04:44 +0100

    Roder med træninger. Plus ændring af stort/lille bogstav i medlemmer.jsx

--- fbcmanager-frontend/src/pages/traening.js
+++ fbcmanager-frontend/src/pages/traening.js
@@ -9,6 +9,24 @@ const Traening = () => {
       <Navbar />
       <div className="body">
         <h1>Træning</h1>
+          <p>Dine træninger:</p>
+          <table>
+              <tr>
+                  <th>Dato</th>
+                  <th>Klokkeslæt</th>
+                  <th>Træningstype</th>
+                  <th>Træner</th>
+                  <th>Note</th>
+              </tr>
+              <tr>
+                  <td>11-03-2022</td>
+                  <td>08:00</td>
+                  <td>Backend</td>
+                  <td>Daniel</td>
+                  <td>Forelæsnings tid, husk at læse op</td>
+              </tr>
+          </table>
+          <p>Søg efter træninger</p>
       </div>
     </div>
   );
