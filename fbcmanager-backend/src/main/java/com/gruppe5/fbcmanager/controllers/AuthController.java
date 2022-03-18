67ab9df3694ab285de4346828b3ea1e18ba37383
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2022-03-18 14:44:31 +0100

    auth

--- fbcmanager-backend/src/main/java/com/gruppe5/fbcmanager/controllers/AuthController.java
+++ fbcmanager-backend/src/main/java/com/gruppe5/fbcmanager/controllers/AuthController.java
@@ -37,82 +37,88 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-  RoleRepository roleRepository;
-    @Autowired
-    PasswordEncoder encoder;
-    @Autowired
-    JwtUtils jwtUtils;
+        @Autowired
+        AuthenticationManager authenticationManager;
+        @Autowired
+        UserRepository userRepository;
+        @Autowired
+        RoleRepository roleRepository;
+        @Autowired
+        PasswordEncoder encoder;
+        @Autowired
+        JwtUtils jwtUtils;
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
-                        loginRequest.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
-                .body(new UserInfoResponse(userDetails.getId(),
-                        userDetails.getUsername(),
-                        userDetails.getEmail(),
-                        roles));
-    }
+        @PostMapping("/signin")
+        public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+                Authentication authentication = authenticationManager
+                                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                                                loginRequest.getPassword()));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+                ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+                List<String> roles = userDetails.getAuthorities().stream()
+                                .map(item -> item.getAuthority())
+                                .collect(Collectors.toList());
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-      if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-        return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
-      }
-      if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-        return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
-      }
-      // Create new user's account
-      UserEntity user = new UserEntity(signUpRequest.getUsername(),
-                           signUpRequest.getEmail(),
-                           encoder.encode(signUpRequest.getPassword()));
-      Set<String> strRoles = signUpRequest.getRole();
-      Set<RoleEntity> roles = new HashSet<>();
-      if (strRoles == null) {
-        RoleEntity userRole = roleRepository.findByName(ERole.ROLE_USER)
-            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-        roles.add(userRole);
-      } else {
-        strRoles.forEach(role -> {
-          switch (role) {
-          case "admin":
-          RoleEntity adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(adminRole);
-            break;
-          case "mod":
-          RoleEntity modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(modRole);
-            break;
-          default:
-          RoleEntity userRole = roleRepository.findByName(ERole.ROLE_USER)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-          }
-        });
-      }
-      user.setRoles(roles);
-      userRepository.save(user);
-      return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-    }
+                System.out.println(roles);
 
-    @PostMapping("/signout")
-    public ResponseEntity<?> logoutUser() {
-        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(new MessageResponse("You've been signed out!"));
-    }
+                return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                                .body(new UserInfoResponse(userDetails.getId(),
+                                                userDetails.getUsername(),
+                                                userDetails.getEmail(),
+                                                roles));
+        }
+
+        @PostMapping("/signup")
+        public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+                if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+                        return ResponseEntity.badRequest()
+                                        .body(new MessageResponse("Error: Username is already taken!"));
+                }
+                if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+                        return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+                }
+                UserEntity user = new UserEntity(signUpRequest.getUsername(),
+                                signUpRequest.getEmail(),
+                                encoder.encode(signUpRequest.getPassword()));
+                Set<String> strRoles = signUpRequest.getRole();
+                Set<RoleEntity> roles = new HashSet<>();
+                if (strRoles == null) {
+                        RoleEntity userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                } else {
+                        strRoles.forEach(role -> {
+                                switch (role) {
+                                        case "admin":
+                                                RoleEntity adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                                                .orElseThrow(() -> new RuntimeException(
+                                                                                "Error: Role is not found."));
+                                                roles.add(adminRole);
+                                                break;
+                                        case "mod":
+                                                RoleEntity modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                                                .orElseThrow(() -> new RuntimeException(
+                                                                                "Error: Role is not found."));
+                                                roles.add(modRole);
+                                                break;
+                                        default:
+                                                RoleEntity userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                                                .orElseThrow(() -> new RuntimeException(
+                                                                                "Error: Role is not found."));
+                                                roles.add(userRole);
+                                }
+                        });
+                }
+                user.setRoles(roles);
+                userRepository.save(user);
+                return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        }
+
+        @PostMapping("/signout")
+        public ResponseEntity<?> logoutUser() {
+                ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+                return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                                .body(new MessageResponse("You've been signed out!"));
+        }
 }
