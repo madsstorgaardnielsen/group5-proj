f35bad706eac4eaf9a0d8d2cf78919728dbedecc
Marcus <mkottosen@gmail.com>
2022-03-28 16:33:35 +0200

    RightColumn looks better and added bulletin board cards

--- fbcmanager-frontend/src/components/RightColumn.js
+++ fbcmanager-frontend/src/components/RightColumn.js
@@ -1,35 +1,48 @@
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
 
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import {CardActionArea} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import UserPic from "../res/img/events/eventPic.jpg";
-import DropdownIcon from "../res/img/navbar/home.svg";
 
-
-
+import "../scss/style.scss";
 
 
 export default function RightColumn() {
     return(
-        <nav className="RightColumn">
-            <Avatar alt="Mette Frederiksen" src={UserPic} variant="rounded"/>
-            <p>Mette Frederiksen</p>
-            <p>Medlemsskab: Aktiv</p>
-            <p>U12</p>
-            <p>Opslagstavle</p>
+        <div className="right_column">
+            <div className="column_top">
+                <div>
+                    <div className="avatar_dropdown">
+                        <Avatar alt="Profile Picture" src={UserPic} variant="rounded"/>
+                    </div>
+                    <div className="avatar_dropdown">
+                        <DropdownMenu/>
+                    </div>
+                </div>
+                <p className="right_align">Mette Frederiksen</p>
+                <p className="right_align">Medlemsskab: Aktiv</p>
+                <p className="right_align">U12</p>
+            </div>
 
-            <DropdownMenu/>
-
-        </nav>
+            <div className="bulletin_card_grid">
+                <p className="bulletin_header">Opslagstavle</p>
+                <FormRow/>
+            </div>
+        </div>
     )
 }
 
 
-
+//Inspiration from mui.com/components/buttons/
 function DropdownMenu() {
 
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -50,7 +63,7 @@ function DropdownMenu() {
                 aria-expanded={open ? 'true' : undefined}
                 onClick={handleClick}
             >
-                +
+                "arrow"
             </Button>
             <Menu
                 id="fade-menu"
@@ -69,6 +82,50 @@ function DropdownMenu() {
         </div>
     );
 }
+
+const numberOfCards = 4;
+function FormRow() {
+    return (
+        <React.Fragment>
+                <section className="bulletin_card_grid">
+                    {Array.from({ length: numberOfCards }, (_, i) => <span key={i}>
+                            <EventCard/>
+                        </span>)}
+                </section>
+        </React.Fragment>
+    );
+}
+
+function EventCard() {
+    const navigate = useNavigate()
+    const toHome = () => {
+        navigate('/home')
+    }
+
+    return (
+        <Card sx={{ maxWidth: 400 }} className="bulletin_card">
+            <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                    Pølsehorn og snacks
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Tag dine forældre med til pølsehorn og snacks ved klubhuset på onsdag!
+                </Typography>
+
+
+                <div className="grid-container">
+                    <div>
+                        <Typography variant="subtitle2" color="text.secondary">
+                            <p className="bulletin_date">29/9 kl. 13:00</p>
+                        </Typography></div>
+
+                </div>
+
+            </CardContent>
+        </Card>
+    );
+}
+
 
 
 
