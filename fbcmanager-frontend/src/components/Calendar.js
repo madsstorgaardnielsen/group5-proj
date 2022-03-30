b5da922833ded487a479ae8359a7300d9e469317
Kongsbak99 <victoreskongsbak@gmail.com>
2022-03-30 09:13:09 +0200

    Next practice card done static. Calendar initiated.

---
+++ fbcmanager-frontend/src/components/Calendar.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import "../scss/calendar.scss";
+
+class Calendar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewType: "Week",
+            durationBarVisible: false,
+            weekStarts: 0,
+            cellHeight: 10,
+            locale: "da-dk",
+            heightSpec: "Full",
+            //height: 1000,
+            dayBeginsHour: 20,
+        
+            // TODO: THESE EVENTS SHOULD BE LOADED AS A LIST OF JSON OBJECT FROM THE DATABASE.
+            events: [
+                {
+                    id: 1,
+                    text: "Træning U18",
+                    start: "2022-03-30T90:30:00",
+                    end: "2022-03-30T11:00:00"
+                },
+                {
+                    id: 2,
+                    text: "Træning Senior",
+                    start: "2022-03-30T17:00:00",
+                    end: "2022-03-30T20:00:00"
+                },
+                {
+                    id: 3,
+                    text: "Træning U18",
+                    start: "2022-04-02T16:30:00",
+                    end: "2022-04-02T18:00:00"
+                },
+                {
+                    id: 4,
+                    text: "Træning U18",
+                    start: "2022-04-03T10:30:00",
+                    end: "2022-04-03T13:00:00"
+                },
+                {
+                    id: 5,
+                    text: "Træning U18",
+                    start: "2022-04-01T16:30:00",
+                    end: "2022-04-01T19:00:00"
+                }
+            ]
+        };
+    }
+
+    render() {
+        const {...config} = this.state;
+        return (
+            <div>
+                <DayPilotCalendar
+                {...config}
+                />
+            </div>
+        );
+    }
+}
+
+export default Calendar;
--- fbcmanager-frontend/src/pages/home.js
+++ fbcmanager-frontend/src/pages/home.js
@@ -1,49 +1,86 @@
-import React from "react";
+import React, {useState } from "react";
 import Navbar from "../components/Navbar";
+import Calendar from "../components/Calendar";
 import "../scss/style.scss"
 import "../scss/homePage.scss"
 
+//import Calendar from 'react-calendar'
+//import 'react-calendar/dist/Calendar.css'
 
-const Home = () => {
+
+//Images
+import team from "../res/img/homepage/team.svg"
+import participants from "../res/img/homepage/participants.svg"
+import location from "../res/img/homepage/location.svg"
+import coach from "../res/img/homepage/coach.svg"
+import react from "react";
+
+
+
+
+function Home() {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div>
       <Navbar />
       <div className="body">
         <div className="grid-container-home">
+
+          {/* XXXXXX MIDDLE COLLUMN START XXXXXX*/}
           <div className="grid-item-home">
-            
             {/* next practice */}
             <div className="next-practice">
-              <p className="h">Næste træning</p>
-              <h1>Træning for seniorer</h1>
-
+              <section className="next-practice-container">
+                <p className="h">Næste træning</p>
+                <h1>Træning for seniorer</h1>
               {/* info container */}
-              <div className="practice-info-container">
-                <section>
-                  <p>25 oktober</p>
-                  <h3>Mandag</h3>
-                  <p>16:00 - 17:30</p>
-                </section>
-                <section>
-                  <p><img></img></p>
-                </section>
-                <section>
-                  <p>test</p>
-                </section>
-              </div>
-
+                <div className="practice-info-container">
+                  <section className="pi-date">
+                    <p>25 oktober</p>
+                    <h3>Mandag</h3>
+                    <p>16:00 - 17:30</p>
+                  </section>
+                  <section>
+                    <span>
+                      <img src={team}></img> 
+                      <p>U18</p>
+                    </span>
+                    <span>
+                      <img src={location}></img>
+                      <p> Bane C</p>
+                    </span>
+                  </section>
+                  <section>
+                    <span>
+                      <img src={participants}></img> 
+                      <p>8 / 12</p>
+                    </span>
+                    <span>
+                      <img src={coach}></img>
+                      <p> Ekkart Kindler</p>
+                    </span>
+                  </section>
+                  
+                </div>
+              </section>
+              <section  className="pi-last-column"></section>
             </div>
 
             {/* Calendar */}
             <div className="home-calendar">
-
+              <Calendar />
             </div>
 
             {/* Events */}
-            <div>
+            <div className="home-events">
 
             </div>
           </div>
+          {/* XXXXXX MIDDLE COLLUMN END XXXXXX*/}
+
+
+          {/* XXXXXX RIGHT COLLUMN START XXXXXX*/}
           <div className="grid-item-home">
             {/* Profile */}
             <div>
@@ -55,6 +92,7 @@ const Home = () => {
 
             </div>
           </div>
+          {/* XXXXXX MIDDLE COLLUMN END XXXXXX*/}
         
         </div>
       </div>
