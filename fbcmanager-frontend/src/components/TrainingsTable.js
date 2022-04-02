cee1948709687ae92433b51104f8b45e8c7d9888
MarieSeindal <s185363@student.dtu.dk>
2022-04-02 20:06:18 +0200

    Training page ready for presentation

--- fbcmanager-frontend/src/components/TrainingsTable.js
+++ fbcmanager-frontend/src/components/TrainingsTable.js
@@ -16,15 +16,14 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 // Table inspired by MUI. https://mui.com/components/tables/
 
-function trainingstable(date, time, fat, carbs, protein, price, description) {
+function trainingstable(date, timeStart, timeEnd, location, team) {
     return {
         date,
-        time,
-        fat,
-        carbs,
-        protein,
-        price,
-        description,
+        timeStart,
+        timeEnd,
+        location,
+        team,
+
     };
 }
 
@@ -44,13 +43,11 @@ function Row(props) {
                         {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                     </IconButton>
                 </TableCell>
-                <TableCell component="th" scope="row">
-                    {row.date}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell component="th" scope="row">{row.date}</TableCell>
+                <TableCell >{row.timeStart}</TableCell>
+                <TableCell >{row.timeEnd}</TableCell>
+                <TableCell >{row.location}</TableCell>
+                <TableCell >{row.team}</TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -58,7 +55,7 @@ function Row(props) {
                         <Box sx={{ margin: 1 }}>
                             <Typography variant="h6" gutterBottom component="div">
                                 Description:
-                                <text> (Insert text from databse). Fun :P This training is focusing on enhancing leg muscles. Remember, dont forget leg day!</text>
+                                <text> In today training session we are going to the forrest. Bring your inner nature-spirit and a bottle of water. </text>
                             </Typography>
                         </Box>
                     </Collapse>
@@ -70,22 +67,18 @@ function Row(props) {
 
 Row.propTypes = {
     row: PropTypes.shape({
-        calories: PropTypes.number.isRequired,
-        carbs: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
+        timeStart: PropTypes.string.isRequired,
+        timeEnd: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
         date: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        protein: PropTypes.number.isRequired,
     }).isRequired,
 };
 
 const rows = [
-    trainingstable('15-03-2022', 159, 6.0, 24, 4.0, 3.99),
-    trainingstable('15-03-2022', 237, 9.0, 37, 4.3, 4.99),
-    trainingstable('17-03-2022', 262, 16.0, 24, 6.0, 3.79),
-    trainingstable('29-03-2022', 305, 3.7, 67, 4.3, 2.5),
-    trainingstable('26-04-2022', 356, 16.0, 49, 3.9, 1.5),
+    trainingstable('15-03-2022', '10:00', '11:00', 'Holte Station', 'U14'), //One traing in a line
+    trainingstable('15-03-2022', '10:00', '14:00', 'Anker Engelunds Vej nr. 1', 'U23'), //Date, timeS, timeE, Location, Team
+    trainingstable('17-03-2022', '22:00', '00:00', 'Lyngby svømmehal', 'U40')
 ];
 
 export default function CollapsibleTable() {
@@ -96,10 +89,10 @@ export default function CollapsibleTable() {
                     <TableRow>
                         <TableCell/>
                         <TableCell>Date</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell >Time Start</TableCell>
+                        <TableCell >Time End&nbsp; </TableCell>
+                        <TableCell >Location&nbsp; </TableCell>
+                        <TableCell >Team&nbsp; </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
