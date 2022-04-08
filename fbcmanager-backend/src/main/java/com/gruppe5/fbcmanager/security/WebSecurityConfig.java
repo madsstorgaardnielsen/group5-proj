cb93ffbae3a0616fe5b14f6f8abf321ca752ec37
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2022-04-08 11:16:46 +0200

    fixed auth bug

--- fbcmanager-backend/src/main/java/com/gruppe5/fbcmanager/security/WebSecurityConfig.java
+++ fbcmanager-backend/src/main/java/com/gruppe5/fbcmanager/security/WebSecurityConfig.java
@@ -56,8 +56,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .antMatchers("/**").permitAll()
+                
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
