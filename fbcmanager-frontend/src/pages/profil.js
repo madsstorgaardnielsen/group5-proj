b260aabfdcbc33e2380c9ddae2b7e89006608835
Swipez1337 <frederiklundsbjerg@hotmail.com>
2022-04-24 16:36:03 +0200

    layout and functionality update for profile, and gitignore node modules

--- fbcmanager-frontend/src/pages/profil.js
+++ fbcmanager-frontend/src/pages/profil.js
@@ -1,65 +1,177 @@
 import React from "react";
 import "../scss/profilePage.scss";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import CollapsibleTable from "../components/TrainingsTable";
 import profile from "../res/img/profile/tmp-profile.png";
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
 
-function Profil () {
+function Profil() {
     return (
         <div>
             <Helmet>
                 <title>Profile | NemSport</title>
             </Helmet>
-            <Navbar />
+
+            <Navbar/>
+
             <div className="profile-info">
-                <section className="profile-pic-section" >
+                <section className="profile-pic-section">
                     <a href="/profil"><img target={"_blank"} src={profile} className="profile-pic"/></a>
                 </section>
+
                 <div>
                     {}
                     <section>
-                        <h4>Contact information</h4>
-                        <p style={{fontWeight: "bold"}}>Name</p>
-                        <p>Mette Frederiksen</p>
-                        <p style={{fontWeight: "bold"}}>Address</p>
-                        <p>Vej 1, 2300 København S</p>
-                        <p style={{fontWeight: "bold"}}>E-mail</p>
-                        <p>mette@frederiksen.dk</p>
-                        <p style={{fontWeight: "bold"}}>Phone number</p>
-                        <p>+4570707070</p>
+
+                        <form>
+                            <h4>Contact information</h4>
+
+                            <div className="form-row">
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputAddress">First name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="inputAddress"
+                                        placeholder="John"
+                                        value={"Mette"}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputAddress">Surname</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="inputAddress"
+                                        placeholder="Doe"
+                                        value={"Frederiksen"}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputEmail4">Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="inputEmail4"
+                                        placeholder="Email"
+                                        value={"mette@frederiksen.dk"}
+                                    />
+                                </div>
+
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputAddress">Phone no.</label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        id="inputAddress"
+                                        placeholder=""
+                                        value={70707070}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputAddress">Address</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="inputAddress"
+                                        placeholder="1234 Main St"
+                                        value={"Landemærket 11"}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputCity">City</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="inputCity"
+                                        placeholder={"City name"}
+                                        value={"København K"}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputZip">Zip code</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="inputZip"
+                                        placeholder={"1234"}
+                                        value={"1119"}
+                                    />
+                                </div>
+
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="inputBirthdate">Birthday</label>
+                                    <input
+                                        type="date"
+                                        className="form-control"
+                                        id="inputZip"
+                                        //placeholder={2000-11-11}
+                                        //value={2000/11/11}
+                                    />
+                                </div>
+
+                            </div>
+
+                        </form>
+
                         <span>
-                    <p>Subcontents</p>
-                    <p>Subsubcontents</p>
-                  </span>
-                        <button type="button">Change information</button>
-                        <button type="button">Change password</button>
+                            <div className="tilmeldBtn">
+                                <button type="submit" className="btn btn-primary">
+                                    Update information
+                                </button>
+                                &nbsp;  &nbsp;  &nbsp;
+                                <button type="submit" className="btn btn-primary">
+                                    Change password
+                                </button>
+                            </div>
+                        </span>
 
                     </section>
+
                     {}
+
                     <section>
+
                         <h4>My training sessions</h4>
-                        <p>Contents</p>
+
+
                         <span>
-                    <p>Subcontents</p>
-                    <p>Subsubcontents</p>
-                  </span>
+                        </span>
+
                         <button type="button">See complete history</button>
+
                     </section>
+
                     {}
+
                     <section>
+
                         <h4>My statistics</h4>
-                        <p>Contents</p>
+
+                        <div className="centerStat">
+                            <p id={"numpractices"} style={{fontWeight: "bold"}}>Number of practices</p>
+                        </div>
+
+                        <div className="centerStat">
+                            <p id={"numpractices_no"}>150 </p>
+                        </div>
+
                         <span>
-                    <p>Subcontents</p>
-                    <p>Subsubcontents</p>
-                  </span>
+                        </span>
+
                     </section>
+
                 </div>
             </div>
         </div>
-
 
 
     );
